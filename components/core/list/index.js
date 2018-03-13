043635767bc597299989a58a63a37c4801cc7173
Carlos Lopez Jr <clshortfuse@gmail.com>
2018-03-13 16:10:52 -0400

    list: use rounded element for ripple

--- components/core/list/index.js
+++ components/core/list/index.js
@@ -1,21 +1,3 @@
-/**
- * @param {HTMLElement} element
- * @param {MouseEvent|PointerEvent} event
- * @return {void}
- */
-function updateRipplePosition(element, event) {
-  if (!event.pointerType && !event.detail) {
-    // Ripple from center
-    element.style.setProperty('left', '0');
-    element.style.setProperty('top', '0');
-    return;
-  }
-  const x = event.offsetX - (element.clientWidth / 2.0);
-  const y = event.offsetY - (element.clientHeight / 2.0);
-  element.style.setProperty('left', `${x}px`);
-  element.style.setProperty('top', `${y}px`);
-}
-
 class List {
   /**
    * @param {Element} element
@@ -59,13 +41,39 @@ class ListRow {
     if (!this.ripple) {
       const ripple = document.createElement('div');
       ripple.classList.add('mdw-ripple');
+
+      const rippleInner = document.createElement('div');
+      rippleInner.classList.add('mdw-ripple__inner');
+
+      ripple.appendChild(rippleInner);
       this.element.insertBefore(ripple, this.element.firstChild);
       this.ripple = ripple;
+      this.rippleInner = rippleInner;
     }
     this.element.setAttribute('mdw-js-ripple', '');
     this.element.addEventListener('click', (event) => {
-      updateRipplePosition(this.ripple, event);
+      this.updateRipplePosition(event);
     });
+  }
+
+  /**
+   * @param {MouseEvent|PointerEvent} event
+   * @return {void}
+   */
+  updateRipplePosition(event) {
+    if (event.target !== this.element && event.target !== this.ripple) {
+      return;
+    }
+    if (!event.pointerType && !event.detail) {
+      // Ripple from center
+      this.rippleInner.style.removeProperty('left');
+      this.rippleInner.style.removeProperty('top');
+      return;
+    }
+    const x = event.offsetX
+    const y = event.offsetY
+    this.rippleInner.style.setProperty('left', `${x}px`);
+    this.rippleInner.style.setProperty('top', `${y}px`);
   }
 
   /**
