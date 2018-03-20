f6fba3d9e9b3415159bff83593a8479b49acb0ab
Carlos Lopez Jr <clshortfuse@gmail.com>
2018-03-20 11:44:24 -0400

    button: reimplement focus-ring

--- components/core/button/index.js
+++ components/core/button/index.js
@@ -22,7 +22,7 @@ class Button {
    */
   constructor(element) {
     this.element = element;
-    const rippleElements = element.getElementsByClassName('.mdw-ripple');
+    const rippleElements = element.getElementsByClassName('mdw-ripple');
     this.ripple = rippleElements && rippleElements[0];
     if (!this.ripple) {
       const ripple = document.createElement('div');
@@ -30,10 +30,38 @@ class Button {
       this.element.insertBefore(ripple, this.element.firstChild);
       this.ripple = ripple;
     }
-    this.element.setAttribute('mdw-js-ripple', '');
+    const innerRippleElements = this.ripple.getElementsByClassName('mdw-ripple__inner');
+    this.rippleInner = innerRippleElements && innerRippleElements[0];
+    if (!this.rippleInner) {
+      const rippleInner = document.createElement('div');
+      rippleInner.classList.add('mdw-ripple__inner');
+      this.ripple.appendChild(rippleInner);
+      this.rippleInner = rippleInner;
+    }
+    this.element.setAttribute('mdw-ripple', '');
     this.element.addEventListener('click', (event) => {
-      updateRipplePosition(this.ripple, event);
+      this.updateRipplePosition(event);
     });
+  }
+
+  /**
+   * @param {MouseEvent|PointerEvent} event
+   * @return {void}
+   */
+  updateRipplePosition(event) {
+    if (event.target !== this.element && event.target !== this.ripple) {
+      return;
+    }
+    if (!event.pointerType && !event.detail) {
+      // Ripple from center
+      this.rippleInner.style.removeProperty('left');
+      this.rippleInner.style.removeProperty('top');
+      return;
+    }
+    const x = event.offsetX;
+    const y = event.offsetY;
+    this.rippleInner.style.setProperty('left', `${x}px`);
+    this.rippleInner.style.setProperty('top', `${y}px`);
   }
 }
 
