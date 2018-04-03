e0cfca2e30412f09f68a7ed237cd9c49326caa49
Carlos Lopez Jr <clshortfuse@gmail.com>
2018-04-03 13:02:28 -0400

    textfield: convert date back to UTC on get value

--- components/core/textfield/index.js
+++ components/core/textfield/index.js
@@ -103,15 +103,14 @@ class TextField {
     }
     const type = this.input.hasAttribute('type') && this.input.getAttribute('type').toLowerCase();
     switch (type) {
-      case 'date':
-        return this.input.valueAsDate;
-      case 'datetime-local':
       case 'number':
       case 'range':
         return this.input.valueAsNumber;
+      case 'date':
+      case 'datetime-local':
       case 'time':
-        if (!this.input.value) {
-          return this.input.value;
+        if (this.input.value == null) {
+          return null;
         }
         return new Date((this.input.valueAsDate.getTimezoneOffset() * 60 * 1000)
           + this.input.valueAsNumber);
