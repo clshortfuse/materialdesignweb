994528c5ca37130101a218347077acb7ee2ff859
Carlos Lopez Jr <clshortfuse@gmail.com>
2018-04-16 15:16:51 -0400

    docs: update darkmode cookie

--- docs/src/menuoptions.js
+++ docs/src/menuoptions.js
@@ -1,20 +1,40 @@
+
+import * as cookies from './cookies';
+
+const darkAttribute = 'black dark';
+const lightAttribute = 'white light';
+
+/**
+ * @param {Element} element
+ * @param {boolean} value
+ * @return {void}
+ */
+function setDarkMode(element, value) {
+  if (value) {
+    document.body.setAttribute('mdw-theme-fill', darkAttribute);
+    element.removeAttribute('mdw-inactive');
+    // element.setAttribute('mdw-active', '');
+    // Poor visibility even though spec says 70% opacity
+  } else {
+    document.body.setAttribute('mdw-theme-fill', lightAttribute);
+    element.setAttribute('mdw-inactive', '');
+    // element.removeAttribute('mdw-active');
+  }
+  cookies.setItem('darkmode', value ? 'true' : 'false', 365);
+}
 /**
  * @param {Element} element
  * @return {void}
  */
 function setupDarkMode(element) {
-  const darkAttribute = 'black dark';
-  const lightAttribute = 'white light';
+  if (cookies.getItem('darkmode') === 'true') {
+    setDarkMode(element, true);
+  }
   element.addEventListener('click', () => {
     if (document.body.getAttribute('mdw-theme-fill') === darkAttribute) {
-      document.body.setAttribute('mdw-theme-fill', lightAttribute);
-      element.setAttribute('mdw-inactive', '');
-      element.removeAttribute('mdw-active');
+      setDarkMode(element, false);
     } else {
-      document.body.setAttribute('mdw-theme-fill', darkAttribute);
-      element.removeAttribute('mdw-inactive');
-      // element.setAttribute('mdw-active', '');
-      // Poor visibility even though spec says 70% opacity
+      setDarkMode(element, true);
     }
   });
 }
