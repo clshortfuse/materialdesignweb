a80556cd3389b0cbaaa0400430cfab06ebe0fa7b
Carlos Lopez Jr <clshortfuse@gmail.com>
2018-04-23 08:16:19 -0400

    docs: show defaults on layout page

--- docs/src/core/layout.js
+++ docs/src/core/layout.js
@@ -29,9 +29,28 @@ function setupInteractions() {
     });
   }
   const layout = document.querySelector('.mdw-layout.docs-layout');
-  const checkbox = document.querySelector('.mdw-navdrawer__content input');
-  checkbox.addEventListener('change', () => {
-    if (checkbox.checked) {
+  const [useDefaultsCheckbox, useGridCheckbox ] = document.querySelectorAll('.mdw-navdrawer__content input');
+
+  useDefaultsCheckbox.addEventListener('change', () => {
+    if (useDefaultsCheckbox.checked) {
+      layout.removeAttribute('mdw-margin');
+      layout.removeAttribute('mdw-gutter');
+      layout.removeAttribute('mdw-margin-tablet');
+      layout.removeAttribute('mdw-gutter-tablet');
+      layout.removeAttribute('mdw-margin-mobile');
+      layout.removeAttribute('mdw-gutter-mobile');
+    } else {
+      layout.setAttribute('mdw-margin', '24');
+      layout.setAttribute('mdw-gutter', '24');
+      layout.setAttribute('mdw-margin-tablet', '16');
+      layout.setAttribute('mdw-gutter-tablet', '16');
+      layout.setAttribute('mdw-margin-mobile', '8');
+      layout.setAttribute('mdw-gutter-mobile', '8');
+    }
+  });
+
+  useGridCheckbox.addEventListener('change', () => {
+    if (useGridCheckbox.checked) {
       layout.setAttribute('mdw-grid', '');
     } else {
       layout.removeAttribute('mdw-grid');
