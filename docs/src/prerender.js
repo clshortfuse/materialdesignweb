ab8bc788c8357872ce4142e8026cfa13619e00c1
Carlos Lopez Jr <clshortfuse@gmail.com>
2018-04-24 08:10:53 -0400

    toolbar: add translucent status bar support for ios web apps

--- docs/src/prerender.js
+++ docs/src/prerender.js
@@ -2,10 +2,25 @@ import * as cookies from './cookies';
 import { setRTLMode, setDarkMode, setFontSize } from './menuoptions';
 
 const useRTLMode = cookies.getItem('rtlmode') === 'true';
-setRTLMode(useRTLMode);
-
 const useDarkMode = cookies.getItem('darkmode') === 'true';
-setDarkMode(useDarkMode);
-
 const fontsize = cookies.getItem('fontsize');
+
+/** @return {boolean} */
+function checkIsStandaloneIOS() {
+  if (!navigator.standalone) {
+    return false;
+  }
+  const ua = navigator.userAgent.toLowerCase();
+  const isIPhone = /iphone/.test(ua);
+  const isIPad = /ipad/.test(ua);
+  return (isIPhone || isIPad);
+}
+
+setRTLMode(useRTLMode);
+setDarkMode(useDarkMode);
 setFontSize(fontsize);
+
+if (checkIsStandaloneIOS()) {
+  document.documentElement.setAttribute('mdw-ios', '');
+  document.documentElement.setAttribute('mdw-standalone', '');
+}
