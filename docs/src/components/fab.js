2e650727fbaf3aad2f11a8abda9456d980315881
Carlos Lopez Jr <clshortfuse@gmail.com>
2018-05-24 00:12:44 -0400

    docs: add fab options

--- docs/src/components/fab.js
+++ docs/src/components/fab.js
@@ -13,6 +13,37 @@ function initializeMdwComponents() {
   for (let i = 0; i < components.length; i += 1) {
     Button.attach(components.item(i));
   }
+  document.querySelector('input[name="hover"]').addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+      document.querySelector('.js .mdw-fab__speed-dial').setAttribute('mdw-hover', '');
+    } else {
+      document.querySelector('.js .mdw-fab__speed-dial').removeAttribute('mdw-hover');
+    }
+  });
+  document.querySelector('input[name="type"][value="regular"]').addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+      document.querySelector('.js .mdw-fab__button').removeAttribute('mdw-mini');
+      document.querySelector('.js .mdw-fab__button').removeAttribute('mdw-extended');
+    }
+  });
+  document.querySelector('input[name="type"][value="mini"]').addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+      document.querySelector('.js .mdw-fab__button').setAttribute('mdw-mini', '');
+      document.querySelector('.js .mdw-fab__button').removeAttribute('mdw-extended');
+    }
+  });
+  document.querySelector('input[name="type"][value="extended"]').addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+      document.querySelector('.js .mdw-fab__button').removeAttribute('mdw-mini');
+      document.querySelector('.js .mdw-fab__button').setAttribute('mdw-extended', '');
+    }
+  });
+  document.querySelector('input[name="type"][value="extended-hover"]').addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+      document.querySelector('.js .mdw-fab__button').removeAttribute('mdw-mini');
+      document.querySelector('.js .mdw-fab__button').setAttribute('mdw-extended', 'hover');
+    }
+  });
 }
 
 initializeMdwComponents();
