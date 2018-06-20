95a67da06c74cdc220871c22d0648f554488cf84
Carlos Lopez Jr <clshortfuse@gmail.com>
2018-06-20 18:54:45 -0400

    docs: update fab to use mdw-layout

--- docs/src/components/layout.js
+++ docs/src/components/layout.js
@@ -1,4 +1,5 @@
 import { setupMenuOptions } from '../menuoptions';
+import { attachEventListener } from '../sample-utils';
 
 
 const colspans = [
@@ -19,6 +20,82 @@ function onLayoutItemClick(target) {
   target.firstElementChild.textContent = colspan.toString();
 }
 
+/**
+ * @param {Event} event
+ * @return {void}
+ */
+function onOptionChange(event) {
+  const { name, value, checked } = event.target;
+  const layout = document.querySelector('.mdw-layout.docs-layout');
+
+  switch (name) {
+    case 'default-margins':
+      if (checked) {
+        layout.removeAttribute('mdw-margin');
+        layout.removeAttribute('mdw-gutter');
+        layout.removeAttribute('mdw-margin-tablet');
+        layout.removeAttribute('mdw-gutter-tablet');
+        layout.removeAttribute('mdw-margin-mobile');
+        layout.removeAttribute('mdw-gutter-mobile');
+      } else {
+        layout.setAttribute('mdw-margin', '24');
+        layout.setAttribute('mdw-gutter', '24');
+        layout.setAttribute('mdw-margin-tablet', '16');
+        layout.setAttribute('mdw-gutter-tablet', '16');
+        layout.setAttribute('mdw-margin-mobile', '8');
+        layout.setAttribute('mdw-gutter-mobile', '8');
+      }
+      break;
+    case 'top-margin':
+      if (checked) {
+        layout.setAttribute('mdw-margin-top', '');
+      } else {
+        layout.removeAttribute('mdw-margin-top');
+      }
+      break;
+    case 'bottom-margin':
+      if (checked) {
+        layout.setAttribute('mdw-margin-bottom', '');
+      } else {
+        layout.removeAttribute('mdw-margin-bottom');
+      }
+      break;
+    case 'stretch':
+      if (checked) {
+        layout.setAttribute('mdw-stretch', '');
+      } else {
+        layout.removeAttribute('mdw-stretch');
+      }
+      break;
+    case 'center':
+      if (checked) {
+        layout.setAttribute('mdw-center', '');
+      } else {
+        layout.removeAttribute('mdw-center');
+      }
+      break;
+    case 'style':
+      switch (value) {
+        case 'grid':
+          layout.setAttribute('mdw-grid', '');
+          break;
+        case 'flex':
+          layout.removeAttribute('mdw-grid');
+          break;
+        default:
+      }
+      break;
+    case 'dense':
+      if (checked) {
+        layout.setAttribute('mdw-dense', '');
+      } else {
+        layout.removeAttribute('mdw-dense');
+      }
+      break;
+    default:
+  }
+}
+
 /** @return {void} */
 function setupInteractions() {
   const layoutItems = document.getElementsByClassName('mdw-layout__item');
@@ -28,76 +105,11 @@ function setupInteractions() {
       onLayoutItemClick(item);
     });
   }
-  const layout = document.querySelector('.mdw-layout.docs-layout');
-  const inputs = document.querySelectorAll('.mdw-navdrawer__content input');
-  const useDefaultsCheckbox = inputs[0];
-  const useFlexRadio = inputs[1];
-  const useStretchCheckbox = inputs[2];
-  const useCenterCheckbox = inputs[3];
-  const useGridRadio = inputs[4];
-  const useDenseCheckbox = inputs[5];
-
-  useDefaultsCheckbox.addEventListener('change', () => {
-    if (useDefaultsCheckbox.checked) {
-      layout.removeAttribute('mdw-margin');
-      layout.removeAttribute('mdw-gutter');
-      layout.removeAttribute('mdw-margin-tablet');
-      layout.removeAttribute('mdw-gutter-tablet');
-      layout.removeAttribute('mdw-margin-mobile');
-      layout.removeAttribute('mdw-gutter-mobile');
-    } else {
-      layout.setAttribute('mdw-margin', '24');
-      layout.setAttribute('mdw-gutter', '24');
-      layout.setAttribute('mdw-margin-tablet', '16');
-      layout.setAttribute('mdw-gutter-tablet', '16');
-      layout.setAttribute('mdw-margin-mobile', '8');
-      layout.setAttribute('mdw-gutter-mobile', '8');
-    }
-  });
-
-  useStretchCheckbox.addEventListener('change', () => {
-    if (useStretchCheckbox.checked) {
-      layout.setAttribute('mdw-stretch', '');
-    } else {
-      layout.removeAttribute('mdw-stretch');
-    }
-  });
-
-  useCenterCheckbox.addEventListener('change', () => {
-    if (useCenterCheckbox.checked) {
-      layout.setAttribute('mdw-center', '');
-    } else {
-      layout.removeAttribute('mdw-center');
-    }
-  });
-
-  useFlexRadio.addEventListener('change', () => {
-    if (useFlexRadio.checked) {
-      layout.removeAttribute('mdw-grid');
-      useStretchCheckbox.removeAttribute('disabled');
-      useCenterCheckbox.removeAttribute('disabled');
-      useDenseCheckbox.setAttribute('disabled', '');
-    }
-  });
-
-  useGridRadio.addEventListener('change', () => {
-    if (useGridRadio.checked) {
-      layout.setAttribute('mdw-grid', '');
-      useStretchCheckbox.setAttribute('disabled', '');
-      useCenterCheckbox.setAttribute('disabled', '');
-      useDenseCheckbox.removeAttribute('disabled');
-    }
-  });
-
-  useDenseCheckbox.addEventListener('change', () => {
-    if (useDenseCheckbox.checked) {
-      layout.setAttribute('mdw-dense', '');
-    } else {
-      layout.removeAttribute('mdw-dense');
-    }
-  });
-
-  
+  attachEventListener(
+    document.querySelectorAll('input[name]'),
+    'change',
+    onOptionChange
+  );
 }
 
 setupInteractions();
