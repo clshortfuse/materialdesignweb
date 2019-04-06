e9a0db3e45d0229d9815cbd0539c99a15f1d9b88
Carlos Lopez Jr <clshortfuse@gmail.com>
2019-04-05 21:12:19 -0400

    fix(transition): scale border-radius

--- core/transition/index.js
+++ core/transition/index.js
@@ -1,6 +1,6 @@
-import { nextTick } from '../dom';
-
 // https://material.io/design/navigation/navigation-transitions.html
+
+import { nextTick } from '../dom';
 
 /**
  * @param {string} px
@@ -14,10 +14,50 @@ function pxToInteger(px) {
 }
 
 /**
+ * @param {string} stringValue
+ * @param {number} width
+ * @param {number} height
+ * @return {{horizontal:number, vertical:number}}
+ */
+function convertBorderRadius(stringValue, width, height) {
+  /**
+   * @param {string} value
+   * @param {number} length
+   * @return {number}
+   */
+  function parsePercentage(value, length) {
+    const numValue = pxToInteger(value);
+    if (value.indexOf('%') === -1) {
+      return numValue;
+    }
+    return (numValue * length / 100.0);
+  }
+
+  if (!stringValue) {
+    return {
+      horizontal: 0,
+      vertical: 0,
+    };
+  }
+
+  const split = stringValue.split(' ');
+  if (split.length === 1) {
+    return {
+      horizontal: parsePercentage(stringValue, width),
+      vertical: parsePercentage(stringValue, height),
+    };
+  }
+  return {
+    horizontal: parsePercentage(split[0], width),
+    vertical: parsePercentage(split[1], height),
+  };
+}
+
+/**
  * @param {HTMLElement} targetElement
  * @param {HTMLElement} originElement
  * @param {CSSStyleDeclaration} [originStyle]
- * @return {string}
+ * @return {Object}
  */
 export function getShapeTransform(targetElement, originElement, originStyle) {
   const originRect = originElement.getBoundingClientRect();
@@ -39,23 +79,37 @@ export function getShapeTransform(targetElement, originElement, originStyle) {
     originHeight -= (paddingTop + paddingBottom);
   }
 
-  return [
-    `translateX(${originX - targetRect.left}px)`,
-    `translateY(${originY - targetRect.top}px)`,
-    `scaleX(${originWidth / targetRect.width})`,
-    `scaleY(${originHeight / targetRect.height})`,
-  ].join(' ');
+  return {
+    originWidth,
+    originHeight,
+    translateX: originX - targetRect.left,
+    translateY: originY - targetRect.top,
+    scaleX: originWidth / targetRect.width,
+    scaleY: originHeight / targetRect.height,
+    toString() {
+      return [
+        `translateX(${this.translateX}px)`,
+        `translateY(${this.translateY}px)`,
+        `scaleX(${this.scaleX})`,
+        `scaleY(${this.scaleY})`,
+      ].join(' ');
+    },
+  };
 }
 
 /**
  * @param {HTMLElement} element
+ * @param {string} property
  * @param {function(TransitionEvent):void} callback
  * @param {number} [expectedDuration]
  * @return {function} removeListener
  */
-function buildTransitionEndListener(element, callback, expectedDuration) {
+function buildTransitionEndListener(element, property, callback, expectedDuration) {
   let hasEnded = false;
   const onTransitionEnd = (event) => {
+    if (event.propertyName !== property) {
+      return;
+    }
     hasEnded = true;
     element.removeEventListener('transitionend', onTransitionEnd);
     callback(event);
@@ -67,7 +121,7 @@ function buildTransitionEndListener(element, callback, expectedDuration) {
       if (!hasEnded) {
         callback(null);
       }
-    }, expectedDuration);
+    }, expectedDuration + 500);
   }
   return () => {
     element.removeEventListener('transitionend', onTransitionEnd);
@@ -79,11 +133,15 @@ function buildTransitionEndListener(element, callback, expectedDuration) {
  * @param {HTMLElement} toShapeElement
  * @param {HTMLElement} fromContentElement
  * @param {HTMLElement} toContentElement
- * @return {function():void} revertFromShape()
+ * @param {number} [duration=250|300] (250ms collapse or 300ms expand)
+ * @param {boolean} [revertFrom=false] Revert from elements on completion
+ * @return {function():void} revertAllElements()
  */
 export function transitionElement(
   fromShapeElement, toShapeElement,
-  fromContentElement, toContentElement
+  fromContentElement, toContentElement,
+  duration,
+  revertFrom = false
 ) {
   const toStyleSyle = window.getComputedStyle(toShapeElement);
 
@@ -91,75 +149,223 @@ export function transitionElement(
   const contentTransform = getShapeTransform(toContentElement, fromContentElement);
 
   const transitionProperties = [
+    'transition',
     'transform-origin',
     'transform',
+    'background-color',
     'border-radius',
     'box-shadow',
+    'z-index',
     'filter',
+    'opacity',
+    'visibility',
   ];
+
+  const oldFromShapeProperties = {};
+  const oldToShapeProperties = {};
+  const oldFromContentProperties = {};
+  const oldToContentProperties = {};
+  const newFromShapeProperties = {};
+  transitionProperties.forEach((prop) => {
+    oldFromShapeProperties[prop] = fromShapeElement.style.getPropertyValue(prop);
+    oldToShapeProperties[prop] = toShapeElement.style.getPropertyValue(prop);
+    oldFromContentProperties[prop] = fromContentElement.style.getPropertyValue(prop);
+    oldToContentProperties[prop] = toContentElement.style.getPropertyValue(prop);
+    if (prop === 'border-radius') {
+      // Border-radius has to be scaled
+      const topLeft = convertBorderRadius(
+        toStyleSyle.getPropertyValue('border-top-left-radius'),
+        shapeTransform.originWidth,
+        shapeTransform.originHeight
+      );
+      const topRight = convertBorderRadius(
+        toStyleSyle.getPropertyValue('border-top-right-radius'),
+        shapeTransform.originWidth,
+        shapeTransform.originHeight
+      );
+      const bottomLeft = convertBorderRadius(
+        toStyleSyle.getPropertyValue('border-bottom-left-radius'),
+        shapeTransform.originWidth,
+        shapeTransform.originHeight
+      );
+      const bottomRight = convertBorderRadius(
+        toStyleSyle.getPropertyValue('border-bottom-right-radius'),
+        shapeTransform.originWidth,
+        shapeTransform.originHeight
+      );
+      const horizontalRadii = [
+        `${topLeft.horizontal / shapeTransform.scaleX}px`,
+        `${topRight.horizontal / shapeTransform.scaleX}px`,
+        `${bottomLeft.horizontal / shapeTransform.scaleX}px`,
+        `${bottomRight.horizontal / shapeTransform.scaleX}px`,
+      ].join(' ');
+      const verticalRadii = [
+        `${topLeft.vertical / shapeTransform.scaleY}px`,
+        `${topRight.vertical / shapeTransform.scaleY}px`,
+        `${bottomLeft.vertical / shapeTransform.scaleY}px`,
+        `${bottomRight.vertical / shapeTransform.scaleY}px`,
+      ].join(' ');
+      newFromShapeProperties[prop] = `${horizontalRadii} / ${verticalRadii}`;
+    } else {
+      newFromShapeProperties[prop] = toStyleSyle.getPropertyValue(prop);
+    }
+  });
+
+  fromShapeElement.setAttribute('mdw-transition-busy', '');
+  toShapeElement.setAttribute('mdw-transition-busy', '');
+  toContentElement.setAttribute('mdw-transition-busy', '');
+  fromContentElement.setAttribute('mdw-transition-busy', '');
 
   // Hide toContentElement and overlay over fromContentElement immediately
   toContentElement.style.setProperty('opacity', '0');
   toContentElement.style.setProperty('transition', 'none');
   toContentElement.style.setProperty('transform-origin', '0 0 0');
   toContentElement.style.setProperty('transform', contentTransform);
-
-  const originalProperties = {};
-  transitionProperties.forEach((prop) => {
-    originalProperties[prop] = fromShapeElement.style.getPropertyValue(prop);
-  });
+  toContentElement.style.setProperty('z-index', '9');
 
   fromContentElement.style.setProperty('opacity', '1');
 
   fromShapeElement.style.setProperty('transition', 'none');
   fromShapeElement.style.setProperty('transform-origin', '0 0 0');
   fromShapeElement.style.setProperty('transform', 'none');
+  fromShapeElement.style.setProperty('z-index', '8');
+
+  toShapeElement.style.setProperty('background-color', 'transparent');
+  toShapeElement.style.setProperty('filter', 'none');
+  toShapeElement.style.setProperty('box-shadow', 'none');
 
   const revertFunction = () => {
-    Object.keys(originalProperties).forEach((prop) => {
-      fromShapeElement.style.setProperty(prop, originalProperties[prop]);
+    transitionProperties.forEach((prop) => {
+      if (oldFromShapeProperties[prop] == null) {
+        fromShapeElement.style.removeProperty(prop);
+      } else {
+        fromShapeElement.style.setProperty(prop, oldFromShapeProperties[prop]);
+      }
+      if (oldToShapeProperties[prop] == null) {
+        toShapeElement.style.removeProperty(prop);
+      } else {
+        toShapeElement.style.setProperty(prop, oldToShapeProperties[prop]);
+      }
+      if (oldFromContentProperties[prop] == null) {
+        fromContentElement.style.removeProperty(prop);
+      } else {
+        fromContentElement.style.setProperty(prop, oldFromContentProperties[prop]);
+      }
+      if (oldToContentProperties[prop] == null) {
+        toContentElement.style.removeProperty(prop);
+      } else {
+        toContentElement.style.setProperty(prop, oldToContentProperties[prop]);
+      }
     });
-    fromShapeElement.style.removeProperty('visibility');
-    fromContentElement.style.removeProperty('transition');
-    fromContentElement.style.removeProperty('opacity');
   };
 
   nextTick(() => {
-    const transformTransition = transitionProperties
-      .filter(prop => prop !== 'transform-origin')
-      .map(prop => `${prop} 300ms cubic-bezier(0.4, 0.0, 0.2, 1) 0s`)
-      .join(', ');
-    const fadeInTransition = 'opacity 210ms cubic-bezier(0.0, 0.0, 0.2, 1) 90ms';
-    const fadeOutTransition = 'opacity 90ms cubic-bezier(0.0, 0.0, 0.2, 1) 0ms';
-
-    fromShapeElement.style.setProperty('transition', transformTransition);
-    transitionProperties.forEach((prop) => {
-      if (prop === 'transform') {
-        fromShapeElement.style.setProperty('transform', shapeTransform);
-      } else if (prop === 'transform-origin') {
-        fromShapeElement.style.setProperty('transform-origin', '0 0 0');
-      } else {
-        fromShapeElement.style.setProperty(prop, toStyleSyle.getPropertyValue(prop));
-      }
-    });
-
     nextTick(() => {
-      buildTransitionEndListener(fromShapeElement, () => {
-        fromShapeElement.style.setProperty('visibility', 'hidden');
+      let calculatedDuration = duration;
+      if (calculatedDuration == null) {
+        if (shapeTransform.scaleX * shapeTransform.scaleY >= 1) {
+          calculatedDuration = 300; // expand
+        } else {
+          calculatedDuration = 250; // collapse
+        }
+      }
+      const totalDuration = calculatedDuration.toString(10);
+      const fadeInTime = (calculatedDuration * 0.70).toString(10);
+      const fadeOutTime = (calculatedDuration * 0.30).toString(10);
+      const transformTransition = transitionProperties
+        .filter(prop => prop !== 'transform-origin' && prop !== 'opacity' && prop !== 'transition')
+        .map(prop => `${prop} ${totalDuration}ms cubic-bezier(0.4, 0.0, 0.2, 1) 0s`)
+        .join(', ');
+      const fadeInTransition = `opacity ${fadeInTime}ms cubic-bezier(0.0, 0.0, 0.2, 1) ${fadeOutTime}ms`;
+      const fadeOutTransition = [
+        `opacity ${fadeOutTime}ms cubic-bezier(0.0, 0.0, 0.2, 1) 0ms`,
+        `visibility ${fadeOutTime}ms cubic-bezier(0.0, 0.0, 0.2, 1) 0ms`,
+      ].join(', ');
+
+      fromShapeElement.style.setProperty('transition', transformTransition);
+      transitionProperties.forEach((prop) => {
+        if (prop === 'transform') {
+          fromShapeElement.style.setProperty('transform', shapeTransform);
+        } else if (prop === 'transition') {
+          // noop;
+        } else if (prop === 'transform-origin') {
+          fromShapeElement.style.setProperty('transform-origin', '0 0 0');
+        } else if (prop === 'z-index') {
+          fromShapeElement.style.setProperty('z-index', '8');
+        } else if (prop === 'visibility') {
+          fromShapeElement.style.setProperty('visibility', 'hidden');
+        } else if (newFromShapeProperties[prop] == null) {
+          fromShapeElement.style.removeProperty(prop);
+        } else {
+          fromShapeElement.style.setProperty(prop, newFromShapeProperties[prop]);
+        }
       });
+
+      nextTick(() => {
+        buildTransitionEndListener(fromShapeElement, 'transform', () => {
+          transitionProperties.forEach((prop) => {
+            if (prop === 'transition') {
+              toShapeElement.style.setProperty('transition', 'none');
+              toContentElement.style.setProperty('transition', 'none');
+            } else {
+              if (oldToShapeProperties[prop] == null) {
+                toShapeElement.style.removeProperty(prop);
+              } else {
+                toShapeElement.style.setProperty(prop, oldToShapeProperties[prop]);
+              }
+              if (oldToContentProperties[prop] == null) {
+                toContentElement.style.removeProperty(prop);
+              } else {
+                toContentElement.style.setProperty(prop, oldToContentProperties[prop]);
+              }
+            }
+
+            if (prop === 'visibility' && !revertFrom) {
+              return;
+            }
+            if (oldFromContentProperties[prop] == null) {
+              fromContentElement.style.removeProperty(prop);
+            } else {
+              fromContentElement.style.setProperty(prop, oldFromContentProperties[prop]);
+            }
+            if (oldFromShapeProperties[prop] == null) {
+              fromShapeElement.style.removeProperty(prop);
+            } else {
+              fromShapeElement.style.setProperty(prop, oldFromShapeProperties[prop]);
+            }
+          });
+
+          nextTick(() => {
+            if (oldToShapeProperties.transition == null) {
+              toShapeElement.style.removeProperty('transition');
+            } else {
+              toShapeElement.style.setProperty('transition', oldToShapeProperties.transition);
+            }
+            if (oldToContentProperties.transition == null) {
+              toContentElement.style.removeProperty('transition');
+            } else {
+              toContentElement.style.setProperty('transition', oldToContentProperties.transition);
+            }
+            fromShapeElement.removeAttribute('mdw-transition-busy');
+            toShapeElement.removeAttribute('mdw-transition-busy');
+            toContentElement.removeAttribute('mdw-transition-busy');
+            fromContentElement.removeAttribute('mdw-transition-busy');
+          });
+        }, calculatedDuration);
+      });
+
+      // Fade out fromContent while shaping into new content
+      fromContentElement.style.setProperty('transition', fadeOutTransition);
+      fromContentElement.style.setProperty('opacity', '0');
+      fromContentElement.style.setProperty('visibility', 'hidden');
+
+      toContentElement.style.setProperty('transition', [
+        transformTransition,
+        fadeInTransition,
+      ].join(', '));
+      toContentElement.style.setProperty('opacity', '1');
+      toContentElement.style.setProperty('transform', 'none');
     });
-
-
-    // Fade out fromContent while shaping into new content
-    fromContentElement.style.setProperty('transition', fadeOutTransition);
-    fromContentElement.style.setProperty('opacity', '0');
-
-    toContentElement.style.setProperty('transition', [
-      transformTransition,
-      fadeInTransition,
-    ].join(', '));
-    toContentElement.style.setProperty('opacity', '1');
-    toContentElement.style.setProperty('transform', 'none');
   });
 
   return revertFunction;
