43cf80285e145dd8e4b97cf27b97a96aebc83e26
Carlos Lopez Jr <clshortfuse@gmail.com>
2022-09-05 19:14:03 -0400

    chore(ripple): drop needless CSS variables

--- core/ripple/MDWRipple.js
+++ core/ripple/MDWRipple.js
@@ -50,9 +50,11 @@ export default class MDWRipple extends MDWOverlay {
     }
 
     const hypotenuse = Math.sqrt((width * width) + (height * height));
-    this.innerElement.style.setProperty('--mdw-ripple-radius', `${hypotenuse}px`);
-    this.innerElement.style.setProperty('--mdw-ripple-x', xPos);
-    this.innerElement.style.setProperty('--mdw-ripple-y', yPos);
+    const size = `${hypotenuse}px`;
+    this.innerElement.style.setProperty('width', size);
+    this.innerElement.style.setProperty('height', size);
+    this.innerElement.style.setProperty('left', xPos);
+    this.innerElement.style.setProperty('top', yPos);
   }
 
   /**
