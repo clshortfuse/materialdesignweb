db752bcabaec893dbbbb8639389b1d2a247208f3
Carlos Lopez Jr <clshortfuse@gmail.com>
2022-09-19 14:26:08 -0400

    divider: fix class name

--- components/divider/MDWDivider.js
+++ components/divider/MDWDivider.js
@@ -2,7 +2,7 @@ import MDWContainer from '../../core/container/MDWContainer.js';
 
 import styles from './MDWDivider.css' assert { type: 'css' };
 
-export default class MDWIcon extends MDWContainer {
+export default class MDWDivider extends MDWContainer {
   static elementName = 'mdw-divider';
 
   static styles = [...super.styles, styles];
