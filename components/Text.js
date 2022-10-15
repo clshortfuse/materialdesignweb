2b927271bdc0a88c6bd870babd37eca73638e9da
Carlos Lopez Jr <clshortfuse@gmail.com>
2022-10-15 12:46:53 -0400

    text: refactor css

--- components/Text.js
+++ components/Text.js
@@ -1,11 +1,12 @@
 import CustomElement from './CustomElement.js';
 import styles from './Text.css' assert { type: 'css' };
+import inkStyles from './TextInks.css' assert { type: 'css' };
 
 /** @implements {HTMLSpanElement} */
 export default class Text extends CustomElement {
   static elementName = 'mdw-text';
 
-  static styles = [...super.styles, styles];
+  static styles = [...super.styles, styles, inkStyles];
 
   static fragments = [
     ...super.fragments,
