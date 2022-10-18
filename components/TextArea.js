501355c52a1e848d93ca48a74631ddf6a10f8f3d
Carlos Lopez Jr <clshortfuse@gmail.com>
2022-10-18 19:41:52 -0400

    textarea: add textarea

---
+++ components/TextArea.js
@@ -0,0 +1,207 @@
+import Control from './Control.js';
+import styles from './TextArea.css' assert { type: 'css' };
+import { TextFieldMixin } from './TextFieldMixin.js';
+
+/** @implements {HTMLTextAreaElement} */
+export default class TextArea extends TextFieldMixin(Control) {
+  static elementName = 'mdw-textarea';
+
+  static styles = [...super.styles, styles];
+
+  static controlTagName = 'textarea';
+
+  static controlVoidElement = false;
+
+  static clonedContentAttributes = [
+    ...super.clonedContentAttributes,
+    'cols',
+    'dirname',
+    'maxlength',
+    'minlength',
+    'placeholder',
+    'rows',
+  ];
+
+  #updatingSlot = false;
+
+  #textarea = /** @type {HTMLTextAreaElement} */ (this.refs.control);
+
+  /**
+   * @param {InputEvent} event
+   * @this {HTMLTextAreaElement}
+   * @return {void}
+   */
+  static onControlInput(event) {
+    super.onControlInput(event);
+    /** @type {{host:TextArea}} */ // @ts-ignore Coerce
+    const { host } = this.getRootNode();
+    // if (!host.autosize) return;
+    host.resize();
+  }
+
+  /**
+   * @param {Event} event
+   * @this {HTMLSlotElement}
+   * @return {void}
+   */
+  static onSlotChange(event) {
+    /** @type {{host:TextArea}} */ // @ts-ignore Coerce
+    const { host } = this.getRootNode();
+    const textarea = /** @type {HTMLTextAreaElement} */ (host.refs.control);
+    const previousValue = textarea.defaultValue;
+    // Skip redundancy check, just replace.
+    let lastChild;
+    while ((lastChild = textarea.lastChild) != null) {
+      lastChild.remove();
+    }
+    for (const child of this.assignedNodes()) {
+      textarea.append(child.cloneNode(true));
+    }
+
+    const newValue = textarea.defaultValue;
+    if (previousValue !== newValue) {
+      host.idlChangedCallback('defaultValue', previousValue, newValue);
+    }
+  }
+
+  compose() {
+    const fragment = super.compose();
+    const slot = fragment.getElementById('slot');
+
+    slot.setAttribute('onslotchange', '{~constructor.onSlotChange}');
+
+    fragment.append(slot);
+    return fragment;
+  }
+
+  /** @return {number} */
+  resize() {
+    const textarea = this.#textarea;
+    textarea.style.removeProperty('height');
+    const { lineHeight } = window.getComputedStyle(textarea);
+    this._maxHeight = this.maxRows
+      ? /* css */` calc((${this.maxRows} * ${lineHeight}) + (var(--control__margin-top) + var(--control__padding-top) + var(--control__padding-bottom) + var(--control__margin-bottom)))`
+      : null;
+    if (this.minRows > 1 && textarea.rows < this.minRows) {
+      textarea.rows = this.minRows;
+    } else if (this.maxRows && textarea.rows > this.maxRows) {
+      textarea.rows = this.maxRows;
+    }
+    if (!this.fixed) {
+      while (textarea.scrollHeight > textarea.clientHeight) {
+        if (this.maxRows && textarea.rows === this.maxRows) break;
+        const lastClientHeight = textarea.clientHeight;
+        textarea.rows++;
+        if (lastClientHeight === textarea.clientHeight) {
+          textarea.rows--;
+          break;
+        }
+      }
+      while (textarea.scrollHeight === textarea.clientHeight) {
+        if (textarea.rows === 1) break;
+        if (this.minRows > 1 && textarea.rows === this.minRows) break;
+        const lastClientHeight = textarea.clientHeight;
+        textarea.rows--;
+        if ((lastClientHeight === textarea.clientHeight)
+        || (textarea.scrollHeight > textarea.clientHeight)) {
+          textarea.rows++;
+          break;
+        }
+      }
+    }
+
+    if (textarea.selectionEnd === textarea.value.length) {
+      textarea.scrollTop = textarea.scrollHeight;
+    }
+    this.rows = textarea.rows;
+    return this.rows;
+  }
+
+  /**
+   * @param {string} name
+   * @param {string?} oldValue
+   * @param {string?} newValue
+   */
+  idlChangedCallback(name, oldValue, newValue) {
+    super.idlChangedCallback(name, oldValue, newValue);
+    switch (name) {
+      case 'defaultValueAttr':
+        this.defaultValue = newValue;
+        break;
+      case 'defaultValue':
+        this._value = this.#textarea.value;
+        this.resize();
+        break;
+      case '_maxHeight':
+        this.#textarea.style.setProperty('max-height', newValue);
+        break;
+      case 'minrows':
+      case 'maxrows':
+        this.resize();
+        break;
+      default:
+    }
+  }
+
+  get updatingSlot() { return this.#updatingSlot; }
+
+  get defaultValue() {
+    return this.#textarea.defaultValue;
+  }
+
+  set defaultValue(value) {
+    const previousValue = this.#textarea.defaultValue;
+    this.#textarea.defaultValue = value;
+    if (previousValue !== this.#textarea.defaultValue) {
+      this.idlChangedCallback('defaultValue', previousValue, this.defaultValue);
+    }
+    this.textContent = this.#textarea.defaultValue;
+  }
+
+  // @ts-ignore @override
+  // eslint-disable-next-line class-methods-use-this
+  get type() { return 'textarea'; }
+
+  get textLength() { return this.#textarea.textLength; }
+
+  get select() { return this.#textarea.select; }
+
+  get selectionDirection() { return this.#textarea.selectionDirection; }
+
+  set selectionDirection(value) { this.#textarea.selectionDirection = value; }
+
+  get selectionStart() { return this.#textarea.selectionStart; }
+
+  set selectionStart(value) { this.#textarea.selectionStart = value; }
+
+  get selectionEnd() { return this.#textarea.selectionEnd; }
+
+  set selectionEnd(value) { this.#textarea.selectionEnd = value; }
+
+  get setRangeText() { return this.#textarea.setRangeText; }
+
+  get setSelectionRange() { return this.#textarea.setSelectionRange; }
+
+  connectedCallback() {
+    this.resize();
+  }
+}
+
+TextArea.prototype._maxHeight = TextArea.idl('_maxHeight', { type: 'string' });
+TextArea.prototype.fixed = TextArea.idl('fixed', { type: 'boolean' });
+TextArea.prototype.minRows = TextArea.idl('minRows', { attr: 'minrows', type: 'integer', empty: 0 });
+TextArea.prototype.maxRows = TextArea.idl('maxRows', { attr: 'maxrows', type: 'integer', empty: 0 });
+
+// https://html.spec.whatwg.org/multipage/form-elements.html#the-textarea-element
+
+const DOMString = { onNullish: String };
+TextArea.prototype.cols = TextArea.idl('cols', { type: 'integer', nullable: false });
+TextArea.prototype.dirName = TextArea.idl('dirName', { attr: 'dirname', ...DOMString });
+TextArea.prototype.maxLength = TextArea.idl('maxLength', { attr: 'maxlength', type: 'integer', nullable: false });
+TextArea.prototype.minLength = TextArea.idl('minLength', { attr: 'minlength', type: 'integer', nullable: false });
+TextArea.prototype.placeholder = TextArea.idl('placeholder', DOMString);
+TextArea.prototype.rows = TextArea.idl('rows', { type: 'integer', nullable: false, default: 0 });
+TextArea.prototype.wrap = TextArea.idl('wrap', DOMString);
+
+// Not in spec, but plays nice with HTML linters
+TextArea.prototype.defaultValueAttr = TextArea.idl('defaultValueAttr', { attr: 'value', ...DOMString });
---
+++ components/TextFieldMixin.js
@@ -0,0 +1,115 @@
+import Control from './Control.js';
+import Icon from './Icon.js';
+import styles from './TextFieldMixin.css' assert { type: 'css' };
+
+/**
+ * @param {typeof Control} Base
+ */
+export function TextFieldMixin(Base) {
+  const TextField = class extends Base {
+    static elementName = 'mdw-textbase';
+
+    static styles = [...super.styles, styles];
+
+    #icon = /** @type {Icon} */ (this.refs.icon);
+
+    #trailingIcon = /** @type {Icon} */ (this.refs['trailing-icon']);
+
+    #label = /** @type {HTMLLabelElement} */ (this.refs.label);
+
+    constructor() {
+      super();
+      if (this.icon == null) {
+      // perf: Drop from DOM if not used
+        this.#icon.remove();
+      }
+      if (this.trailingIcon == null) {
+        this.#trailingIcon.remove();
+      }
+    }
+
+    compose() {
+      const fragment = super.compose();
+      const { html } = this;
+      const control = fragment.getElementById('control');
+      control.setAttribute('type', 'text');
+      control.setAttribute('placeholder', '{placeholder}');
+      control.setAttribute('aria-labelledby', 'label-text');
+      control.classList.add('inline');
+      fragment.getElementById('label').append(
+        html`
+          <mdw-icon id=icon aria-hidden=true>{icon}</mdw-icon>
+          <span class=inline id=prefix aria-hidden=true>{inputPrefix}</span>
+          <span class=inline id=suffix aria-hidden=true>{inputSuffix}</span>
+          <mdw-icon id=trailing-icon aria-hidden=true>{trailingIcon}</mdw-icon>
+          <div id=indicator></div>
+          <div id=outline>
+            <div id=gap>
+              <div id=label-text>{label}</div>
+            </div>
+          </div>
+        `,
+      );
+      fragment.getElementById('label-text').append(
+        fragment.getElementById('slot'),
+      );
+      fragment.append(html`
+        <div id=supporting>${({ error, _validationMessage, supporting }) => (error || _validationMessage || supporting) ?? ''}</div>
+      `);
+
+      fragment.getElementById('ripple').remove();
+      return fragment;
+    }
+
+    /**
+     * @param {string} name
+     * @param {string?} oldValue
+     * @param {string?} newValue
+     */
+    attributeChangedCallback(name, oldValue, newValue) {
+      super.attributeChangedCallback(name, oldValue, newValue);
+      if (oldValue == null && newValue == null) return;
+      if (name === 'icon') {
+        if (oldValue == null) {
+          this.#label.append(this.#icon);
+        } else if (newValue == null) {
+          this.#icon.remove();
+        }
+      }
+      if (name === 'trailing-icon') {
+        if (oldValue == null) {
+          this.#label.append(this.#trailingIcon);
+        } else if (newValue == null) {
+          this.#trailingIcon.remove();
+        }
+      }
+    }
+
+    /**
+     * @param {string} name
+     * @param {string?} oldValue
+     * @param {string?} newValue
+     */
+    idlChangedCallback(name, oldValue, newValue) {
+      super.idlChangedCallback(name, oldValue, newValue);
+      if (oldValue == null && newValue == null) return;
+      switch (name) {
+        case 'value':
+        case '_badInput':
+          this._populated = this.value || this._badInput;
+          break;
+        default:
+      }
+    }
+  };
+  TextField.prototype.type = TextField.idl('type', { empty: 'text', nullable: false });
+  TextField.prototype.icon = TextField.idl('icon');
+  TextField.prototype.label = TextField.idl('label');
+  TextField.prototype.inputPrefix = TextField.idl('inputPrefix');
+  TextField.prototype.inputSuffix = TextField.idl('inputSuffix');
+  TextField.prototype.trailingIcon = TextField.idl('trailingIcon');
+  TextField.prototype.supporting = TextField.idl('supporting');
+  TextField.prototype.error = TextField.idl('error');
+  TextField.prototype._populated = TextField.idl('_populated', { attr: 'populated', type: 'boolean' });
+  return TextField;
+}
