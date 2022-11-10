5e193b286ce46da919740ce0537f82f532a9b8b2
Carlos Lopez Jr <clshortfuse@gmail.com>
2022-11-10 16:39:13 -0500

    tooltip: first commit

---
+++ components/Tooltip.js
@@ -0,0 +1,12 @@
+import Container from './Container.js';
+import styles from './Tooltip.css' assert { type: 'css' };
+
+export default class Tooltip extends Container {
+  static elementName = 'mdw-tooltip';
+
+  static ariaRole = 'tooltip';
+
+  static { this.autoRegister(); }
+
+  static styles = [...super.styles, styles];
+}
