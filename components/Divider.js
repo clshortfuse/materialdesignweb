b716bdc45b12ef038b3d1fa72a107277961008b6
Carlos Lopez Jr <clshortfuse@gmail.com>
2022-12-06 14:17:50 -0500

    divider: add vertical idl

--- components/Divider.js
+++ components/Divider.js
@@ -2,11 +2,11 @@ import Container from './Container.js';
 import styles from './Divider.css' assert { type: 'css' };
 
 export default class Divider extends Container {
-  static { this.autoRegister(); }
-
-  static elementName = 'mdw-divider';
+  static { this.autoRegister('mdw-divider'); }
 
   compose() {
     return super.compose().append(styles);
   }
 }
+
+Divider.prototype.vertical = Divider.idl('vertical', 'boolean');
