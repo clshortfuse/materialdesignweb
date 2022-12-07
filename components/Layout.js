8cbe474454d40c5ea6ba04181534e1e5f87b003b
Carlos Lopez Jr <clshortfuse@gmail.com>
2022-12-07 17:17:58 -0500

    layout: extend Container and add gap

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,32 @@
-import CustomElement from '../core/CustomElement.js';
-
+import Container from './Container.js';
 import styles from './Layout.css' assert { type: 'css' };
 
-export default class Layout extends CustomElement {
-  static { this.autoRegister(); }
-
-  static elementName = 'mdw-layout';
+export default class Layout extends Container {
+  static { this.autoRegister('mdw-layout'); }
 
   compose() {
     return super.compose().append(
       styles,
-      '<slot></slot>',
+      this.updateGap,
     );
   }
+
+  /**
+   * @param {Partial<this>} data
+   */
+  updateGap({ gap }) {
+    // TODO: Style slot instead of host
+    if (gap) {
+      this.style.setProperty('--mdw-layout__gap', gap);
+    } else {
+      this.style.removeProperty('--mdw-layout__gap');
+    }
+  }
 }
+
+Layout.prototype.x = Layout.idl('x');
+Layout.prototype.y = Layout.idl('y');
+Layout.prototype.column = Layout.idl('column', 'boolean');
+/** @type {null|'wrap'|'reverse'} */
+Layout.prototype.wrap = Layout.idl('wrap');
+Layout.prototype.gap = Layout.idl('gap');
