551df675f7848ef18f7718bfc2da388fd73a81f7
Carlos Lopez Jr <clshortfuse@gmail.com>
2022-12-07 17:06:18 -0500

    divider: draw as overlay

--- components/Divider.js
+++ components/Divider.js
@@ -1,11 +1,13 @@
-import Container from './Container.js';
 import styles from './Divider.css' assert { type: 'css' };
+import Text from './Text.js';
 
-export default class Divider extends Container {
+export default class Divider extends Text {
   static { this.autoRegister('mdw-divider'); }
 
   compose() {
-    return super.compose().append(styles);
+    const composition = super.compose();
+    composition.template.getElementById('slot').remove();
+    return composition.append(styles);
   }
 }
 
