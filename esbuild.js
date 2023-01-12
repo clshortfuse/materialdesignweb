08a0aafafadba95db2999ad8b979f084ce3b3f5c
Carlos Lopez Jr <clshortfuse@gmail.com>
2023-01-12 11:14:38 -0500

    dev: add esbuild serve

--- esbuild.js
+++ esbuild.js
@@ -1,5 +1,5 @@
 import { writeFileSync } from 'node:fs';
-import { relative } from 'node:path';
+import path, { relative } from 'node:path';
 import process from 'node:process';
 import { brotliCompressSync, gzipSync } from 'node:zlib';
 
@@ -15,6 +15,7 @@ const minifyAll = cliArgs.has('--minify');
 const minifyWhitespace = cliArgs.has('--minify-whitespace');
 const minifyIdentifiers = cliArgs.has('--minify-identifiers');
 const minifySyntax = cliArgs.has('--minify-syntax');
+const serve = cliArgs.has('--serve');
 
 const minify = minifyAll ? { minify: true } : {
   minifyWhitespace,
@@ -22,7 +23,8 @@ const minify = minifyAll ? { minify: true } : {
   minifySyntax,
 };
 
-await esbuild.build({
+/** @type {esbuild.BuildOptions} */
+const buildOptions = {
   entryPoints: ['docs/demo.js'],
   format: 'esm',
   sourcemap: true,
@@ -82,7 +84,31 @@ await esbuild.build({
           writeFileSync('meta.json', JSON.stringify(metafile));
         }
         console.log(info);
+        console.log('Build completed.');
       });
     },
   }],
-});
+};
+
+let server;
+if (serve) {
+  /** @type {esbuild.ServeOptions} */
+  const serveOptions = {
+    port: 5500,
+    servedir: './',
+    onRequest(args) {
+      const localPath = path.join(serveOptions.servedir, args.path);
+      if (localPath === buildOptions.outfile) {
+        // console.log('accessed outfile');
+      }
+    },
+  };
+
+  server = await esbuild.serve(serveOptions, { entryPoints: [] });
+  console.log('esbuild server is listening on port', serveOptions.port);
+}
+
+await Promise.all([
+  server?.wait,
+  esbuild.build(buildOptions),
+]);
