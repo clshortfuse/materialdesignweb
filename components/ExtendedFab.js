c0890b3845101446a79084e679cfb50401e239a2
Carlos Lopez Jr <clshortfuse@gmail.com>
2023-03-17 08:51:56 -0400

    shape: expand outline support

--- components/ExtendedFab.js
+++ components/ExtendedFab.js
@@ -4,4 +4,8 @@ import styles from './ExtendedFab.css' assert { type: 'css' };
 export default Button
   .extend()
   .css(styles)
+  .observe({
+    filled: { empty: 'tonal' },
+    elevated: { type: 'boolean', empty: true },
+  })
   .autoRegister('mdw-extended-fab');
