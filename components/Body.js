540fe7068641514763f0ccc41a963b850b66c50c
Carlos Lopez Jr <clshortfuse@gmail.com>
2023-03-17 08:52:21 -0400

    body: remove themeable redundancy

--- components/Body.js
+++ components/Body.js
@@ -1,10 +1,7 @@
-import ThemableMixin from '../mixins/ThemableMixin.js';
-
 import styles from './Body.css' assert { type: 'css' };
 import Box from './Box.js';
 
 export default Box
-  .mixin(ThemableMixin)
   .extend()
   .css(styles)
   .autoRegister('mdw-body');
