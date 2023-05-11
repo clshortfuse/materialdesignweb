432ba06b3689b239282092c110ee9d5268cc7d6f
Carlos Lopez Jr <clshortfuse@gmail.com>
2023-05-10 20:20:52 -0400

    uid: simplify generator

--- core/uid.js
+++ core/uid.js
@@ -1,12 +1,13 @@
 /** @type {Set<string>} */
 const generatedUIDs = new Set();
 
-/** @return {string} */
-export function generateUID() {
-  const id = Math.random().toString(36).slice(2, 10);
-  if (generatedUIDs.has(id)) {
-    return generateUID();
-  }
+/**
+ * @param {string} [prefix='x'] Prefix all UIDs by string to apply a name or ensure starts with [A-Z] character
+ * @param {number} [n] Maximum number of variations needed. Calculated as 32^n.
+  @return {string} */
+export function generateUID(prefix = 'mdw_', n = 4) {
+  let id;
+  while (generatedUIDs.has(id = Math.random().toString(36).slice(2, n + 2)));
   generatedUIDs.add(id);
-  return id;
+  return `${prefix}${id}`;
 }
