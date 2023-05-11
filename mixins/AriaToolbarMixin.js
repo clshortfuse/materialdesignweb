697af72a194167381b6fea754574e7cec0c09071
Carlos Lopez Jr <clshortfuse@gmail.com>
2023-05-11 09:04:40 -0400

    ariatoolbar: match import filename

--- mixins/AriaToolbarMixin.js
+++ mixins/AriaToolbarMixin.js
@@ -1,11 +1,11 @@
-import KbdNavWidgetMixin from './KeyboardNavMixin.js';
+import KeyboardNavMixin from './KeyboardNavMixin.js';
 
 /**
  * @param {typeof import('../core/CustomElement.js').default} Base
  */
 export default function AriaToolbarMixin(Base) {
   return Base
-    .mixin(KbdNavWidgetMixin)
+    .mixin(KeyboardNavMixin)
     .extend()
     .set({
       ariaOrientationDefault: 'horizontal',
