3b542a1950f1d4534cc44d3f0dfba2c1e68321d3
Carlos Lopez Jr <clshortfuse@gmail.com>
2023-06-28 14:39:55 -0400

    test: test browsers individually

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,12 +1,5 @@
-import { playwrightLauncher } from '@web/test-runner-playwright';
-
 import { axTreePlugin } from './test/axTreePlugin.js';
 
 export default {
-  browsers: [
-    playwrightLauncher({ product: 'chromium' }),
-    playwrightLauncher({ product: 'firefox' }),
-    playwrightLauncher({ product: 'webkit' }),
-  ],
   plugins: [axTreePlugin()],
 };
