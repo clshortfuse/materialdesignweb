e4546a28fa014ef8671ac59452ea6f2fa531333b
Carlos Lopez Jr <clshortfuse@gmail.com>
2023-06-29 16:28:02 -0400

    fix(button): use .value as text fallback

--- components/Button.js
+++ components/Button.js
@@ -54,13 +54,12 @@ export default CustomElement
   .html`
     <mdw-icon mdw-if={hasIcon} id=icon ink={iconInk} disabled={disabledState} outlined={outlined} aria-hidden=true svg={svg} src={src} svg-path={svgPath} view-box={viewBox} icon={icon}></mdw-icon>
     <a mdw-if={href} id=anchor href={href} aria-label="{_computedAriaLabel}"></a>
-    <slot id=slot disabled={disabledState} aria-hidden=false></slot>
+    <slot id=slot disabled={disabledState} aria-hidden=false>{value}</slot>
   `
   .recompose(({ refs: { shape, state, rippleContainer, surface, control } }) => {
     surface.append(shape);
     shape.append(state, rippleContainer);
     shape.setAttribute('filled', '{filled}');
-    control.setAttribute('aria-label', '{computedAriaLabel}');
     control.setAttribute('hidden', '{href}');
     control.setAttribute('role', 'button');
   })
