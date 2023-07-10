333b6628b1a5f3ed93bc5b14691a9d07ad581491
Carlos Lopez Jr <clshortfuse@gmail.com>
2023-07-10 19:00:20 -0400

    test: add card snapshots

---
+++ test/screenshot/Card.test.js
@@ -0,0 +1,35 @@
+import '../../loaders/theme.js';
+import Button from '../../components/Button.js';
+import Card from '../../components/Card.js';
+import Ripple from '../../components/Ripple.js';
+import { addRobotoFont, disableAnimations, generateScreenshotTests } from '../utils.js';
+
+disableAnimations(Button, Card, Ripple);
+await addRobotoFont();
+
+beforeEach(() => document.body.replaceChildren());
+
+describe('mdw-card', () => {
+  generateScreenshotTests('<mdw-card padding=24>Card</mdw-card>', [
+    {
+      enabled: {},
+      disabled: { disabled: true },
+    },
+    {
+      elevated: { elevated: true },
+      filled: { filled: true },
+      outlined: { outlined: true },
+    },
+    {
+      nonactionable: {},
+      actionable: { actionable: true },
+    },
+    {
+      stateless: {},
+      hovered: { ':hover': true, textContent: 'Hovered' },
+      focused: { ':focus': true, textContent: 'Focused' },
+      hovered__focused: { ':hover': true, ':focus': true, textContent: 'Hovered and Focused' },
+      pressed: { ':active': true, textContent: 'Pressed' },
+    },
+  ]);
+});
