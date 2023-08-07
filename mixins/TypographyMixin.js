975ab0cf22f24c55eb30cfb7981ee281867d9e58
Carlos Lopez Jr <clshortfuse@gmail.com>
2023-08-07 13:35:36 -0400

    fix(typography): ignore html whitespace

--- mixins/TypographyMixin.js
+++ mixins/TypographyMixin.js
@@ -1,9 +1,11 @@
+import { ELEMENT_STYLER_TYPE } from '../core/customTypes.js';
+
 /**
  * @param {string} input
  * @return {string}
  */
 function parseSize(input) {
-  if (!input) return '0';
+  if (!input) return '';
   if (input.includes('px')) return input;
   if (input.includes('em')) return input;
   return `calc(${input.replace('sp', '')} * 0.0625rem)`;
@@ -18,63 +20,75 @@ export default function TypographyMixin(Base) {
       textLeading: 'string',
       textPaddingBottom: 'string',
     })
-    .expressions({
-      hasTextPaddingTop({ textPaddingTop, textPadding }) {
-        return Boolean(textPaddingTop ?? textPadding);
+    .observe({
+      _computedTextPaddingTop({ textPaddingTop, textPadding }) {
+        return parseSize(textPaddingTop ?? textPadding);
       },
-      hasTextPaddingBottom({ textPaddingBottom, textPadding }) {
-        return Boolean(textPaddingBottom ?? textPadding);
+      _computedTextPaddingBottom({ textPaddingBottom, textPadding }) {
+        return parseSize(textPaddingBottom ?? textPadding);
       },
-      slotStyle({ textPadding, textPaddingTop, textLeading }) {
-        return `--text-padding-top:${parseSize(textPaddingTop ?? textPadding)};`
-         + `--text-leading:${parseSize(textLeading)}`;
+      _computedTextLeading({ textLeading }) {
+        return parseSize(textLeading);
       },
-      afterStyle({ textPadding, textPaddingBottom }) {
-        return textPaddingBottom === '0' ? null
-          : `--text-padding-bottom:${parseSize(textPaddingBottom ?? textPadding)};`;
+    })
+    .observe({
+      _beforeStyle: {
+        ...ELEMENT_STYLER_TYPE,
+        get({ _computedTextPaddingTop: marginBlockStart, _computedTextLeading: blockSize }) {
+          if (!marginBlockStart && !blockSize) return null;
+          return {
+            target: 'before',
+            styles: {
+              ...(marginBlockStart ? { marginBlockStart } : {}),
+              ...(blockSize ? { blockSize } : {}),
+            },
+          };
+        },
+      },
+      _afterStyle: {
+        ...ELEMENT_STYLER_TYPE,
+        get({ _computedTextPaddingBottom }) {
+          if (!_computedTextPaddingBottom) return null;
+          return {
+            target: 'after',
+            styles: {
+              verticalAlign: `calc(-1 * ${_computedTextPaddingBottom})`,
+            },
+          };
+        },
       },
     })
     .html`
-      <span id=after mdw-if={afterStyle} style={afterStyle}></span>
+      <span id=before mdw-if={!!_beforeStyle}></span>
+      <span id=after mdw-if={!!_afterStyle}></span>
     `
-    .recompose(({ refs: { slot } }) => {
-      slot.setAttribute('style', '{slotStyle}');
-      slot.setAttribute('text-padding-top', '{hasTextPaddingTop}');
-      slot.setAttribute('text-leading', '{textLeading}');
+    .recompose(({ refs: { before, slot } }) => {
+      slot.before(before);
     })
     .css`
       :host {
         display: block;
       }
 
-      #slot[text-padding-top]::before {
-        content: '';
-
+      span {
         display: inline-block;
-        vertical-align: baseline;
+        vertical-align: 0;
+      }
 
+      #slot {
+        display: inline-block;
+      }
+
+      #before {
         block-size: 1.4ex; /* Estimate */
-        margin-block-start: var(--text-padding-top);
       }
       @supports(height:1cap) {
-        #slot[cap-top]::before {
+        #before {
           block-size: 1cap;
         }
       }
 
-      #slot[text-leading]::before {
-        content: '';
-
-        display: inline-block;
-        vertical-align: baseline;
-
-        block-size: var(--text-leading);
-      }
-      
       #after {
-        display: inline-block;
-        vertical-align: calc(-1 * var(--text-padding-bottom));
-
         min-block-size: 1px; /* Safari workaround */
       }
 
