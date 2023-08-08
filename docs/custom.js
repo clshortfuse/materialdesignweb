9d05d23b1a52a888a407e21b0252955a52525632
Carlos Lopez Jr <clshortfuse@gmail.com>
2023-08-08 15:23:15 -0400

    docs: update icons

--- docs/custom.js
+++ docs/custom.js
@@ -1,4 +1,3 @@
-import { unaliased } from '../services/svgAlias.js';
 import { PALETTES } from '../services/theme.js';
 
 import { addIconAliases, reportUnaliasedMaterialSymbols } from './icon-aliases.js';
@@ -8,5 +7,5 @@ PALETTES.push('yellow', 'orange', 'green', 'alias');
 addIconAliases();
 
 setTimeout(async () => {
-  console.debug(JSON.stringify(await reportUnaliasedMaterialSymbols(...unaliased)));
+  console.debug(JSON.stringify(await reportUnaliasedMaterialSymbols()));
 });
