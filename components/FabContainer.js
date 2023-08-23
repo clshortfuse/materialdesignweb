f8fc4cd45e64c56a75e6863935a1ea9e6c90fa54
Carlos Lopez Jr <clshortfuse@gmail.com>
2023-08-23 10:08:12 -0400

    fix(fabcontainer): delegate focus

--- components/FabContainer.js
+++ components/FabContainer.js
@@ -1,7 +1,9 @@
 import CustomElement from '../core/CustomElement.js';
+import DelegatesFocusMixin from '../mixins/DelegatesFocusMixin.js';
 
 export default CustomElement
   .extend()
+  .mixin(DelegatesFocusMixin)
   .html`
     <slot id=slot></slot>
   `
