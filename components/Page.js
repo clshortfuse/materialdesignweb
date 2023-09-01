cf0dc896dee132f9a6af22d4dbffd6921ddfbb02
Carlos Lopez Jr <clshortfuse@gmail.com>
2023-08-31 20:19:04 -0400

    feat(page): add pane switching

--- components/Page.js
+++ components/Page.js
@@ -26,43 +26,43 @@ export default CustomElement
     },
   })
   .observe({
-    expanded: {
-      type: 'boolean',
-      reflect: 'write',
-      get({ paneTwo, paneTwoActive, _lastComputedInlineSize, paneTwoBreakpoint }) {
-        return paneTwo && (paneTwoActive || _lastComputedInlineSize >= paneTwoBreakpoint);
-      },
+    _isMultipane({ _lastComputedInlineSize, paneTwo, paneTwoBreakpoint }) {
+      return Boolean(paneTwo && _lastComputedInlineSize >= paneTwoBreakpoint);
     },
   })
+  .observe({
+    _styles({ _isMultipane, paneOne, paneTwoActive }) {
+      if (_isMultipane) {
+        return `:host{grid-template-columns:${paneOne === 'fixed' ? '360px' : '1fr'} ${this.paneTwo === 'fixed' ? '360px' : '1fr'}}`;
+      }
+      if (paneTwoActive) {
+        return '--host{grid-template-columns: 1fr 0} #slot{display:none}';
+      }
+      return '--host{grid-template-columns: 1fr 0} #two{display:none}';
+    },
+
+  })
   .html`
-    <slot id=slot></slot>
-    <slot id=two name=two hidden={!expanded} ></slot>
+    <style id=styles>{_styles}</style>
+    <slot id=slot mutli-pane={_isMultipane}></slot>
+    <slot id=two name=two mutli-pane={_isMultipane}></slot>
   `
   .css`
     :host {
       --mdw-pane__max-width: 1040px;
       --mdw-pane__margin-inline: 16px;
       --mdw-pane__padding-inline: max(var(--mdw-pane__margin-inline), calc((100% - var(--mdw-pane__max-width)) / 2));
-      --mdw-pane__border-radius: variable-exists;
       --mdw-pane__shape__size: 0;
       display: grid;
       column-gap: var(--mdw-page__margin);
       grid-auto-columns: 1fr;
       grid-auto-flow: column;
-      grid-template-columns: 1fr;
 
       padding: 0;
-
-      
 
       font: var(--mdw-typescale__body-large__font);
       letter-spacing: var(--mdw-typescale__body-large__letter-spacing);
     }
-
-    :host([expanded]) {
-      display: grid;
-    }
-    
 
     @media (min-width: 648px) {
       :host {
@@ -76,16 +76,21 @@ export default CustomElement
       }
     }
 
-    :host([pane-two="fixed"]) {
-      grid-auto-columns: 360px;
+    #slot[multi-pane] {
+      --mdw-pane__padding-inline: 24px;
     }
 
-    :host([expanded][pane-one="fixed"]) {
-      grid-template-columns: 360px;
-    }
+    #two[mutli-pane] {
+      --mdw-pane__padding-inline: 0 24px;
+      position: sticky;
+      inset-block:0;
 
-    #two[hidden] {
-      display: none;
+      display: block;
+
+      overflow-y:auto;
+
+      block-size: 100vh;
+      block-size: 100dvh;
     }
   `
   .overrides({
