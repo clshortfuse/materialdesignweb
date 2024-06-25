8e8abe378625d2acfa873e7e34fd3fb9ca926bad
Carlos Lopez Jr <clshortfuse@gmail.com>
2024-06-25 12:05:57 -0400

    feat(popup): add elevation

--- components/Popup.js
+++ components/Popup.js
@@ -10,4 +10,20 @@ export default CustomElement
   .mixin(ElevationMixin)
   .mixin(ShapeMixin)
   .mixin(PopupMixin)
+  .observe({
+    elevation: 'integer',
+  })
+  .css`
+    :host {
+      filter: var(--mdw-elevation__drop-shadow__0);
+
+      transition: filter 200ms;
+    }
+
+    :host(:where([elevation="1"])) { filter: var(--mdw-elevation__drop-shadow__1); }
+    :host(:where([elevation="2"])) { filter: var(--mdw-elevation__drop-shadow__2); }
+    :host(:where([elevation="3"])) { filter: var(--mdw-elevation__drop-shadow__3); }
+    :host(:where([elevation="4"])) { filter: var(--mdw-elevation__drop-shadow__4); }
+    :host(:where([elevation="5"])) { filter: var(--mdw-elevation__drop-shadow__5); }
+  `
   .autoRegister('mdw-popup');
